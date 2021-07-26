d5da1b2bab319e8426270bfb7140906a6cf64805
baranidj <baranidharanj6295@gmail.com>
2021-07-26 11:08:33 +0530

    task completed

---
+++ src/org/computer/Computer.java
@@ -0,0 +1,15 @@
+package org.computer;
+
+public class Computer {
+	
+	public void computerModel() {
+		System.out.println("enter computer model : L440 lenovo");
+		
+
+	}
+	public static void main(String[] args) {
+		
+	}
+	
+
+}
---
+++ src/org/computer/Desktop.java
@@ -0,0 +1,17 @@
+package org.computer;
+
+public class Desktop extends Computer {
+	
+	private void desktopsize() {
+		System.out.println("my desktop size :\t15*6");
+
+	}
+ public static void main(String[] args) {
+	Desktop d= new Desktop();
+	d.desktopsize();
+	d.computerModel();
+}
+ 
+ 
+ 
+}
